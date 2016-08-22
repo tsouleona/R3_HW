a5f85a412bf86ea44a00032817d65f665fe52cdc
Leona <srt459vn31@gmail.com>
2016-08-22 11:32:25 +0000

    創踩地雷地圖

--- bow2.php
+++ bow2.php
@@ -1,72 +1,17 @@
 <?php
-    for ($i = 0; $i < 40; $i++){
-        do{
-            $bow[$i] = rand(0, 99);
-            for ($j = 0 ; $j < $i ; $j++){
-                if ($bow[$i] == $bow[$j]) {
-                    $bow[$i] = 0;
-                    break;
-                }
-            }
-        }while($bow[$i] == 0);
-    }
-
-    //echo "炸彈位置<br>";
-
-    //個位數前面補0
-    for($i = 0 ; $i < 40 ; $i++)
+    for ($i = 0; $i < 40; $i++)
     {
-        $bow[$i] = str_pad($bow[$i],2,"0",STR_PAD_LEFT);
+        $data_all[$i] = "M";
     }
-    //全部給null
-    for($i=0 ; $i<10 ; $i++)
+
+    for($i = 40 ; $i < 100 ; $i++)
     {
-        for($j=0 ; $j<10 ; $j++)
-        {
-            $origin[$i][$j] = 0;
-        }
+        $data_all[$i] = 0;
     }
 
-    for($k=0;$k<40;$k++)
-    {
-        $x = substr($bow[$k], 0, 1);
-        $y = substr($bow[$k], 1, 1);
+    shuffle($data_all);
 
-        $origin[$x][$y] = "M";
-
-
-    }
-
-    // for($i=0 ; $i<10 ; $i++)
-    // {
-    //     for($j=0 ; $j<10 ; $j++)
-    //     {
-    //         echo $origin[$i][$j]." ";
-    //         if($j == 9)
-    //         {
-    //             echo "<br>";
-    //         }
-    //     }
-    // }
-    //echo "<br>-------------------------------------------------------<br>";
-
-    $data_all =[];
-
-    for($i=0 ; $i<10 ; $i++)
-    {
-        for($j=0 ; $j<10 ; $j++)
-        {
-            $data_all[] = $origin[$i][$j];
-        }
-    }
-    // echo "<br>變一維-------------------------------------------------------<br>";
-    $count = count($data_all);
-    // for($i = 0 ; $i < $count ; $i++)
-    // {
-    //     echo $data_all[$i];
-    // }
-
-    for($i = 0 ; $i < $count ; $i++)
+    for($i = 0 ; $i < 100 ; $i++)
     {
         if(!preg_match("/M/", $data_all[$i]))
         {
@@ -127,20 +72,7 @@
 
         }
     }
-    // echo "<br>計算幾顆炸彈-------------------------------------------------------<br>";
-    // for($i = 0 ; $i < $count ; $i++)
-    // {
-    //     echo $data_all[$i];
-    // }
-    // for($i = 0 ; $i < $count ; $i++)
-    // {
-    //     if(preg_match("/M/", $data_all[$i]))
-    //     {
-    //         $data_all[$i] = "x";
-    //     }
-    // }
-    //echo "<br>";
-    for($k = 0 ; $k < $count; $k++)
+    for($k = 0 ; $k < 100; $k++)
     {
         $x = floor($k/10);
         $y = $k%10;
@@ -155,9 +87,5 @@
         for($j=0 ; $j<11 ; $j++)
         {
             echo $ans[$i][$j];
-            // if(preg_match("/N/", $ans[$i][$j]))
-            // {
-            //     echo "<br>";
-            // }
         }
     }
