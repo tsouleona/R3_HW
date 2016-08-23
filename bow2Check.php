da7bf32c6997a3f6b39ea116819c2553060bc096
Leona <srt459vn31@gmail.com>
2016-08-23 04:14:27 +0000

    檢查10*10的地圖是否正確

---
+++ bow2Check.php
@@ -0,0 +1,145 @@
+<?php
+    $check = $_GET['map'];
+    //判別輸入的數量
+    if($check == null)
+    {
+        echo "您輸入的內容為空";
+        exit;
+    }
+
+    $cutString = explode("N",$check);
+    $trunString = implode($cutString);
+    $original = str_split($trunString);
+    $data_all = $original;
+    //預先判別格式
+    if(count($data_all) < 99)
+    {
+        echo "您輸入的內容長度不足";
+        exit;
+    }
+    $count = @array_count_values($original);
+    if($count['n'] != 0)
+    {
+        echo "N要大寫<br>";
+        exit;
+    }
+    if($count['m'] != 0)
+    {
+        echo "M要大寫<br>";
+        exit;
+    }
+    if($count['N'] != 9)
+    {
+        echo "N的數量不對<br>";
+        exit;
+    }
+    if($count['M'] != 40)
+    {
+        echo "M的數量不對<br>";
+        exit;
+    }
+
+//開始判別對錯
+    for($i = 0 ; $i < 100 ; $i++)
+    {
+        if(!preg_match("/M/", $data_all[$i]))
+        {
+            $data_all[$i] = "0" ;
+        }
+    }
+
+    for($i = 0 ; $i < 100 ; $i++)
+    {
+        if(!preg_match("/M/", $data_all[$i]))
+        {
+            if(($i - 1) % 10 != 9)
+            {
+                if(preg_match("/M/", $data_all[$i - 1]))
+                {
+                    $data_all[$i] = (int)$data_all[$i] + 1;
+                }
+            }
+            if(($i + 1) % 10 != 0)
+            {
+                if(preg_match("/M/", $data_all[$i + 1]))
+                {
+                    $data_all[$i] = (int)$data_all[$i] + 1;
+                }
+            }
+            if(($i - 11) % 10 != 9)
+            {
+                if(preg_match("/M/", $data_all[$i - 11]))
+                {
+                    $data_all[$i] = (int)$data_all[$i] + 1;
+                }
+            }
+            if(($i - 10) > 0)
+            {
+                if(preg_match("/M/", $data_all[$i - 10]))
+                {
+                    $data_all[$i] = (int)$data_all[$i] + 1;
+                }
+            }
+            if(($i - 9) % 10 != 0)
+            {
+                if(preg_match("/M/", $data_all[$i - 9]))
+                {
+                    $data_all[$i] = (int)$data_all[$i] + 1;
+                }
+            }
+
+            if(($i + 9) % 10 != 9)
+            {
+                if(preg_match("/M/", $data_all[$i + 9]))
+                {
+                    $data_all[$i] = (int)$data_all[$i] + 1;
+                }
+            }
+            if(preg_match("/M/", $data_all[$i + 10]))
+            {
+                $data_all[$i] = (int)$data_all[$i] + 1;
+            }
+            if(($i + 11) % 10 != 0)
+            {
+                if(preg_match("/M/", $data_all[$i + 11]))
+                {
+                    $data_all[$i] = (int)$data_all[$i] + 1;
+                }
+            }
+
+        }
+    }
+    for($k = 0 ; $k < 100; $k++)
+    {
+        $x = floor($k/10);
+        $y = $k%10;
+        $ans[$x][$y] = $data_all[$k];
+    }
+    for($i = 0 ; $i < 9 ; $i++)
+    {
+        $ans[$i][10] = "N";
+    }
+    for($i=0 ; $i<11 ; $i++)
+    {
+        for($j=0 ; $j<11 ; $j++)
+        {
+            $ans[$i][$j];
+        }
+    }
+    for($i=0 ; $i<11 ; $i++)
+    {
+        for($j=0 ; $j<11 ; $j++)
+        {
+             $final[] = $ans[$i][$j];
+        }
+    }
+    $stringFinal = implode("", $final);
+    $compare = strcmp($stringFinal, $check);
+    if( $compare == 0)
+    {
+         echo "正確";
+    }
+    elseif($compare != 0)
+    {
+        echo "數字錯誤<br>";
+    }
