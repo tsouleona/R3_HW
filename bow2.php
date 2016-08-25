c0b3460447169a7aa43a0bcb44dcfadf182a31f9
Leona <srt459vn31@gmail.com>
2016-08-25 10:14:04 +0000

    0825

--- bow2.php
+++ bow2.php
@@ -71,14 +71,7 @@
 
         }
     }
-    for($k = 0 ; $k < 100; $k++)
-    {
-        if(preg_match("/M/", $data_all[$k]))
-        {
-            $data_all[$k] = "x";
-        }
 
-    }
     for($k = 0 ; $k < 100; $k++)
     {
         $x = floor($k/10);
@@ -93,10 +86,6 @@
     {
         for($j=0 ; $j<11 ; $j++)
         {
-            echo $ans[$i][$j]." ";
-            if(preg_match("/N/", $ans[$i][$j]))
-            {
-                echo "<br>";
-            }
+            echo $ans[$i][$j];
         }
     }
